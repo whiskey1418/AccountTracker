5cca18baef9f52e6bcb0d275e31a55a53d613465
whiskey1418 <adlawan1418@gmail.com>
2026-09-17 04:00:11 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zsammy-tracker-v13';
+const CACHE_NAME = 'zsammy-tracker-v14';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
